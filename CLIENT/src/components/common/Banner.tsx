96ab2e2a64f25c46261362a18a9071b57f103e91
hao2001vt <nguyenchihao2001vt@gmail.com>
2021-09-29 21:42:48 +0700

    Fix bug

--- CLIENT/src/components/common/Banner.tsx
+++ CLIENT/src/components/common/Banner.tsx
@@ -1,7 +1,5 @@
-import type { NextPage } from 'next';
 import Link from 'next/link';
 import Slider from 'react-slick';
-
 import data from '../../data/banner.json';
 import SampleNextArrow from '../arrowSlider/sampleNextArrow';
 import SamplePrevArrow from '../arrowSlider/samplePreveArrow';
@@ -17,7 +15,7 @@ const settings = {
   nextArrow: <SampleNextArrow />,
   prevArrow: <SamplePrevArrow />,
 };
-const Banner: NextPage = () => {
+const Banner = () => {
   return (
     <div className="banner">
       <Slider {...settings}>
