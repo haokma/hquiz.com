6906d39f331f64f5a5fb48848df5a865be329fe1
hao2001vt <nguyenchihao2001vt@gmail.com>
2021-10-23 06:42:54 +0700

    Api fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const LIMIT = 10;
-export const URL_API = 'https://tracnghiemonline01.herokuapp.com/api';
+export const URL_API = 'https://tracnghiemonline001.herokuapp.com/api';
 
 // http://localhost:5000/api
 // https://tracnghiemonline01.herokuapp.com/api
